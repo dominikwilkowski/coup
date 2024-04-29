b3cbe2fe9f1754314a21a9ed7ac0fd8b1f6ac7f4
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-04-30 06:04:35 +1000

    added on_turn and on_auto_coup to honestbot

--- src/bots/honest_bot.rs
+++ src/bots/honest_bot.rs
@@ -2,7 +2,7 @@
 
 use crate::{
 	bot::{BotInterface, Context},
-	Action,
+	Action, Card,
 };
 
 /// The honest bot will try to take all actions it should take without being too
@@ -15,9 +15,68 @@ impl BotInterface for HonestBot {
 		String::from("HonestBot")
 	}
 
-	// TODO: implement honest bot
+	fn on_turn(&self, context: &Context) -> Action {
+		if context.cards.contains(&Card::Assassin) && context.coins >= 3 {
+			let target =
+				context.other_bots.iter().min_by_key(|bot| bot.cards).unwrap();
+			return Action::Assassination(target.name.clone());
+		} else if context.cards.contains(&Card::Captain) {
+			let target =
+				context.other_bots.iter().max_by_key(|bot| bot.coins).unwrap();
+			return Action::Stealing(target.name.clone());
+		} else if context.cards.contains(&Card::Duke) {
+			return Action::Tax;
+		} else {
+			Action::Income
+		}
+	}
 
-	fn on_turn(&self, _context: &Context) -> Action {
-		Action::Income
+	fn on_auto_coup(&self, context: &Context) -> String {
+		let target = context.other_bots.iter().min_by_key(|bot| bot.cards).unwrap();
+		target.name.clone()
+	}
+
+	fn on_challenge_action_round(
+		&self,
+		_action: &Action,
+		_by: String,
+		_context: &Context,
+	) -> bool {
+		// TODO
+		false
+	}
+
+	fn on_counter(
+		&self,
+		_action: &Action,
+		_by: String,
+		_context: &Context,
+	) -> Option<bool> {
+		// TODO
+		None
+	}
+
+	fn on_challenge_counter_round(
+		&self,
+		_action: &Action,
+		_by: String,
+		_context: &Context,
+	) -> bool {
+		// TODO
+		false
+	}
+
+	fn on_swapping_cards(
+		&self,
+		new_cards: [Card; 2],
+		_context: &Context,
+	) -> [Card; 2] {
+		// TODO
+		new_cards
+	}
+
+	fn on_card_loss(&self, context: &Context) -> Card {
+		// TODO
+		context.cards.clone().pop().unwrap()
 	}
 }
